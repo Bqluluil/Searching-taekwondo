157ffd3641ce00871f464982a4bf5225ed6f1fd8
Bqluluil <93120169+Bqluluil@users.noreply.github.com>
2021-12-27 21:26:28 +0800

    Create tombolsend.php

---
+++ tombolsend.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>coba.php</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="command" placeholder="ketikan sesuatu">
+        <button>send</button>
+        <p align = center>
+</form>
+<?php include 'appcoba.php' ?>
+</body>
+</html>
